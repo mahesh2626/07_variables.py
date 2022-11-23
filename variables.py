fd553600b92d611ff91e2a5f88dfe6feffbfd9dc
mahesh2626 <118418933+mahesh2626@users.noreply.github.com>
2022-11-23 11:25:40 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,29 @@
+a=8
+b=5
+a,b=b,a
+print(a)
+print(b)
+
+print('''variables
+   is''')
+
+"""python"""
+_=7
+print(_)
+
+course='python'
+print('we learning',course)
+
+print('a'.isidentifier())
+print('A'.isidentifier())
+print('3c'.isidentifier())
+print(' '.isidentifier())
+print('_'.isidentifier())
+
+a=7
+b=4
+c=a
+a=b
+b=c
+print(a)
+print(b)
